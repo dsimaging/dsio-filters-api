0cfa5547ba3f0f68bc5760b02db2d97c15a718ec
Selvan Seeman <selvan.seeman@dentsplysirona.com>
2021-05-06 11:06:24 -0700

    Added test code for upload, create image from modality session, delete image and all filters

--- sdk/dotnet/samples/ConsoleApp/Program.cs
+++ sdk/dotnet/samples/ConsoleApp/Program.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Threading.Tasks;
 using DSIO.Filters.Api.Sdk.Types.V1;
 using DSIO.Filters.Api.Sdk.Client.V1;
+using System.Net;
+using System.Net.Http;
 
 namespace ConsoleApp
 {
@@ -36,69 +38,161 @@ namespace ConsoleApp
 
                 if (isAvailable)
                 {
+                    ImageResource imageResource = null;
+
+                    // Test Upload Image
+                    Console.Write("Enter the Image File URL Input Parameter: ");
+                    string uploadImageFileName = Console.ReadLine();
+                    var fileStream = new FileStream(uploadImageFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    var imageContent = new StreamContent(fileStream);
+                    // Upload a new image
+                    service.UploadImage(imageContent, "image/png").ContinueWith(task =>
+                    {
+                        if (task.IsFaulted)
+                        {
+                            Console.WriteLine(task.Exception?.Message);
+                        }
+                        else if (task.IsCompleted)
+                        {
+                            imageResource = task.Result;
+                            Console.WriteLine($"Filters Api V1 service Image Resource: {imageResource.Url}");
+                        }
+                    });
+
+                    // Test Create Image From a Modality Session
+                    Console.Write("Enter the Modality Session Input Parameters: ");
+                    string modalitySessionParam = Console.ReadLine();
+                    ModalitySession modalitySession = Newtonsoft.Json.JsonConvert.DeserializeObject<ModalitySession>(modalitySessionParam);
+                    // Create image from Modality Session
+                    service.CreateImage(modalitySession).ContinueWith(task =>
+                    {
+                        if (task.IsFaulted)
+                        {
+                            Console.WriteLine(task.Exception?.Message);
+                        }
+                        else if (task.IsCompleted)
+                        {
+                            imageResource = task.Result;
+                        }
+                    });
+                    if (imageResource != null)
+                    {
+                        Console.WriteLine($"Filters Api V1 service Image Resource: {imageResource.Url}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Filters Api V1 service Image Resource Not Found");
+                    }
+
+                    // Test Get Image
                     Console.Write("Enter the Image Id: ");
                     var imageId = Console.ReadLine();
-                    var imageResource = await service.GetImage(imageId);
-                    Console.WriteLine($"Filters Api V1 service Image Resource: {imageResource.Url}");
+                    imageResource = await service.GetImage(imageId);
+                    if (imageResource != null)
+                    {
+                        Console.WriteLine($"Filters Api V1 service Image Resource: {imageResource.Url}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Filters Api V1 service Image Resource Not Found");
+                    }
+
+                    // Test Delete Image
+                    Console.Write("Enter the Image Id: ");
+                    imageId = Console.ReadLine();
+                    HttpStatusCode httpStatus = await service.DeleteImage(imageId);
+                    if (httpStatus == HttpStatusCode.OK)
+                    {
+                        Console.WriteLine($"Filters Api V1 service Image Resource: {imageResource.Url}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Filters Api V1 service Image Resource Not Found");
+                    }
 
                     // Test Select Filter
-                    var selectFilterParam = new SelectFilterImageParam
+                    string selectFilteredImageFileName = string.Empty;
+                    Console.Write("Enter the Image Id: ");
+                    imageId = Console.ReadLine();
+                    Console.Write("Enter the Select Filter Input Parameters: ");
+                    string selectFilterParam = Console.ReadLine();
+                    SelectFilterImageParam selectFilterImageParam = Newtonsoft.Json.JsonConvert.DeserializeObject<SelectFilterImageParam>(selectFilterParam);
+                    // Apply Select Filter
+                    service.SelectFilter(imageId, selectFilterImageParam).ContinueWith(task =>
                     {
-                        EnhancementMode = EnhancementMode.EdgePro,
-                        LutInfo = new LutInfo
+                        if (task.IsFaulted)
                         {
-                            Gamma = 2.3,
-                            Slope = 65535,
-                            Offset = 0,
-                            TotalGrays = 4096,
-                            MinimumGray = 3612,
-                            MaximumGray = 418
+                            Console.WriteLine(task.Exception?.Message);
                         }
-                    };
+                        else if (task.IsCompleted)
+                        {
+                            selectFilteredImageFileName = System.Environment.CurrentDirectory + @"\SelectFilteredImage.png";
+                            task.Result.WriteToFile(selectFilteredImageFileName);
+                        }
+                    });
 
                     // Test Supreme Filter
-                    var supremeFilterParam = new SupremeFilterImageParam
+                    string supremeFilteredImageFileName = string.Empty;
+                    Console.Write("Enter the Image Id: ");
+                    imageId = Console.ReadLine();
+                    Console.Write("Enter the Supreme Filter Input Parameters: ");
+                    string supremeFilterParam = Console.ReadLine();
+                    SupremeFilterImageParam supremeFilterImageParam = Newtonsoft.Json.JsonConvert.DeserializeObject<SupremeFilterImageParam>(supremeFilteredImageFileName);
+                    // Apply Select Filter
+                    service.SupremeFilter(imageId, supremeFilterImageParam).ContinueWith(task =>
                     {
-                        TaskName = TaskName.General,
-                        BinningMode = BinningMode.Binned2X2,
-                        Sharpness = 70,
-                        LutInfo = new LutInfo
+                        if (task.IsFaulted)
                         {
-                            Gamma = 2.3,
-                            Slope = 65535,
-                            Offset = 0,
-                            TotalGrays = 4096,
-                            MinimumGray = 3612,
-                            MaximumGray = 418
+                            Console.WriteLine(task.Exception?.Message);
                         }
-                    };
+                        else if (task.IsCompleted)
+                        {
+                            supremeFilteredImageFileName = System.Environment.CurrentDirectory + @"\SupremeFilteredImage.png";
+                            task.Result.WriteToFile(supremeFilteredImageFileName);
+                        }
+                    });
 
                     // Test AE Filter
-                    var omegaFilterParam = new OmegaFilterImageParam
+                    string aeFilteredImageFileName = string.Empty;
+                    Console.Write("Enter the Image Id: ");
+                    imageId = Console.ReadLine();
+                    Console.Write("Enter the Ae Filter Input Parameters: ");
+                    string omegaFilterParam = Console.ReadLine();
+                    OmegaFilterImageParam omegaFilterImageParam = Newtonsoft.Json.JsonConvert.DeserializeObject<OmegaFilterImageParam>(omegaFilterParam);
+                    // Apply ae Filter
+                    service.AeFilter(imageId, omegaFilterImageParam).ContinueWith(task =>
                     {
-                        TaskName = TaskName.General,
-                        Sharpness = 70,
-                        LutInfo = new LutInfo
+                        if (task.IsFaulted)
                         {
-                            Gamma = 2.3,
-                            Slope = 65535,
-                            Offset = 0,
-                            TotalGrays = 4096,
-                            MinimumGray = 3612,
-                            MaximumGray = 418
+                            Console.WriteLine(task.Exception?.Message);
                         }
-                    };
+                        else if (task.IsCompleted)
+                        {
+                            aeFilteredImageFileName = System.Environment.CurrentDirectory + @"\AEFilteredImage.png";
+                            task.Result.WriteToFile(aeFilteredImageFileName);
+                        }
+                    });
 
                     // Test Unmap Filter
-                    var lutInfo = new LutInfo
+                    string UnmapFilteredImageFileName = string.Empty;
+                    Console.Write("Enter the Image Id: ");
+                    imageId = Console.ReadLine();
+                    Console.Write("Enter the Unmap Filter Input Parameters: ");
+                    string unmapFilterParam = Console.ReadLine();
+                    LutInfo lutInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<LutInfo>(unmapFilterParam);
+                    // Apply Unmap Filter
+                    service.UnmapFilter(imageId, lutInfo).ContinueWith(task =>
                     {
-                        Gamma = 2.3,
-                        Slope = 65535,
-                        Offset = 0,
-                        TotalGrays = 4096,
-                        MinimumGray = 3612,
-                        MaximumGray = 418
-                    };
+                        if (task.IsFaulted)
+                        {
+                            Console.WriteLine(task.Exception?.Message);
+                        }
+                        else if (task.IsCompleted)
+                        {
+                            UnmapFilteredImageFileName = System.Environment.CurrentDirectory + @"\UnmapFilteredImage.png";
+                            task.Result.WriteToFile(UnmapFilteredImageFileName);
+                        }
+                    });
 
                     // We are now listening for changes in the Device list. Try
                     // changing the connected sensor of the Simulator to see examples
