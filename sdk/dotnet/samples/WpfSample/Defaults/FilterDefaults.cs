7b97a0b51f6f75f7bdba052dbf461a50977790c3
John Goyette <John.Goyette@dentsplysirona.com>
2021-05-11 11:56:13 -0400

    Fixed FilterDefaults json text

--- sdk/dotnet/samples/WpfSample/Defaults/FilterDefaults.cs
+++ sdk/dotnet/samples/WpfSample/Defaults/FilterDefaults.cs
@@ -14,52 +14,59 @@ namespace WpfSample.Defaults
             Dictionary<FilterType, string> _filterParamList = new Dictionary<FilterType, string>();
 
 
-            _filterParamList.Add(FilterType.Select, @"{
-'enhancementMode': 'edgePro',
-'lutInfo': {
-                'gamma': 2.3,
-'slope': 65535,
-'offset': 0,
-'totalGrays': 4096,
-'minimumGray': 3612,
-'maximumGray': 418
-}
-        }");
+            _filterParamList.Add(FilterType.Select, 
+@"{
+    'enhancementMode': 'edgePro',
+    'lutInfo': 
+    {
+        'gamma': 2.3,
+        'slope': 65535,
+        'offset': 0,
+        'totalGrays': 4096,
+        'minimumGray': 3612,
+        'maximumGray': 418
+    }
+}");
 
-            _filterParamList.Add(FilterType.Supreme, @"{
-  'task': 'general',
-  'binningMode': 'binned2X2',
-  'sharpness': 70,
-  'lutInfo': {
-                'gamma': 2.3,
+            _filterParamList.Add(FilterType.Supreme, 
+@"{
+    'taskName': 'general',
+    'binningMode': 'Unbinned',
+    'sharpness': 70,
+    'lutInfo': 
+    {
+        'gamma': 2.3,
+        'slope': 65535,
+        'offset': 0,
+        'totalGrays': 4096,
+        'minimumGray': 3612,
+        'maximumGray': 418
+    }
+}");
+
+            _filterParamList.Add(FilterType.Ae, 
+@"{
+    'taskName': 'general',
+    'sharpness': 70,
+    'lutInfo': 
+    {
+        'gamma': 2.3,
+        'slope': 65535,
+        'offset': 0,
+        'totalGrays': 4096,
+        'minimumGray': 3612,
+        'maximumGray': 418
+    }
+}");
+
+            _filterParamList.Add(FilterType.Unmap, 
+@"{
+    'gamma': 2.3,
     'slope': 65535,
     'offset': 0,
     'totalGrays': 4096,
     'minimumGray': 3612,
     'maximumGray': 418
-  }
-        }");
-
-            _filterParamList.Add(FilterType.Ae, @"{
-  'task': 'general',
-  'sharpness': 70,
-  'lutInfo': {
-                'gamma': 2.3,
-    'slope': 65535,
-    'offset': 0,
-    'totalGrays': 4096,
-    'minimumGray': 3612,
-    'maximumGray': 418
-  }
-        }");
-
-            _filterParamList.Add(FilterType.Unmap, @"{
-  'gamma': 2.3,
-  'slope': 65535,
-  'offset': 0,
-  'totalGrays': 4096,
-  'minimumGray': 3612,
-  'maximumGray': 418
 }");
 
             return _filterParamList;
