a24d48feb8e46bc5cd246c58309b7f94f743a4d9
Bo, Steven <Steven.Bo@dentsplysirona.com>
2021-11-01 16:58:26 -0400

    S4SP-766 Added LutInfo, BinningMode to ImageResource

---
+++ sdk/dotnet/lib/DSIO.Filters.Api.Sdk.Types/V1/AcquisitionInfo.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace DSIO.Filters.Api.Sdk.Types.V1
+{
+    public class AcquisitionInfo
+    {
+        public BinningMode Binning { get; set; }
+    }
+}
---
+++ sdk/dotnet/lib/DSIO.Filters.Api.Sdk.Types/V1/ImageInfo.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DSIO.Filters.Api.Sdk.Types.V1
+{
+    public class ImageInfo
+    {
+        public AcquisitionInfo AcquisitionInfo { get; set; }
+
+        public LutInfo LutInfo { get; set; }
+    }
+}
